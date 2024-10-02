0abc4eb4a6821dac32cd6c234702e422db8d3dbd
prampey <prudhvi.rampey@gmail.com>
2024-10-02 12:06:38 -0700

    feat: add prime in rust

---
+++ rust/prime/src/main.rs
@@ -0,0 +1,32 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    number: i32,
+}
+
+fn main() {
+    let args = Args::parse();
+    let is_prime = is_prime(args.number);
+    println!("Is prime: {}", is_prime);
+}
+
+fn is_prime(number: i32) -> bool {
+    if number <= 1 {
+        return false;
+    }
+    // Filter out even numbers
+    if number % 2 == 0 {
+        return false;
+    }
+    // Only need to check up to the square root of the number
+    let limit = (number as f64).sqrt() as i32;
+    for i in (3..=limit).step_by(2) {
+        if number % i == 0 {
+            return false;
+        }   
+    }
+    true
+}
