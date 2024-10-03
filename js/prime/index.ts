f203ab8e58f3343b85639633cabf2d3a49a17002
Aashutosh Rathi <aashutosh@stackrlabs.xyz>
2024-10-03 12:29:09 -0700

    refactor: newline and minor var changes

--- js/prime/index.ts
+++ js/prime/index.ts
@@ -1,16 +1,25 @@
 function isPrime(num: number): boolean {
-    if (num <= 1) return false;
-    if (num === 2) return true;
-    if (num % 2 === 0) return false;
-    for (let i = 3; i <= Math.sqrt(num); i += 2) {
-        if (num % i === 0) return false;
-    }
+  if (num <= 1) {
+    return false;
+  }
+  if (num === 2) {
     return true;
+  }
+  if (num % 2 === 0) {
+    return false;
+  }
+
+  const upperLimit = Math.floor(Math.sqrt(num));
+  for (let i = 3; i <= upperLimit; i += 2) {
+    if (num % i === 0) return false;
+  }
+
+  return true;
 }
 
 const input = parseInt(prompt("Enter a number to check if it's prime:") || "");
 if (isNaN(input)) {
-    console.log("Invalid input. Please enter a valid number.");
+  console.log("Invalid input. Please enter a valid number.");
 } else {
-    console.log(`${input} is ${isPrime(input) ? "prime" : "not prime"}.`);
+  console.log(`${input} is ${isPrime(input) ? "prime" : "not prime"}.`);
 }
