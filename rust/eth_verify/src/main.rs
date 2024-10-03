dda4906cb2a8d4e854dd982e99f8f8617d61685e
prampey <prudhvi.rampey@gmail.com>
2024-10-03 16:53:11 -0700

    feat: add eth verify rust

---
+++ rust/eth_verify/src/main.rs
@@ -0,0 +1,17 @@
+use alloy_consensus;
+use alloy_primitives::keccak256;
+use alloy_rpc_types::Block;
+use alloy_rlp;
+use serde_json;
+
+fn main() {
+    let file_path = "block_data.json";
+    let file_content = std::fs::read_to_string(file_path)
+        .expect("Failed to read the file");
+    let s = file_content.as_str();
+    let block = serde_json::from_str::<Block>(s).unwrap();
+    let header: alloy_consensus::Header = block.clone().header.try_into().unwrap();
+    let recomputed_hash = keccak256(alloy_rlp::encode(&header));
+    assert_eq!(recomputed_hash, block.header.hash);
+    println!("Recomputed hash: {}", recomputed_hash);
+}
