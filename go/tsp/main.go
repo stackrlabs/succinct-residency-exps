272e0feb90b56ffa4c9bef91cc74a790cf0d8a52
prampey <prudhvi.rampey@gmail.com>
2024-10-03 13:33:24 -0700

    refactor: vars tsp

--- go/tsp/main.go
+++ go/tsp/main.go
@@ -12,49 +12,49 @@ func main() {
 		{15, 35, 0, 30},
 		{20, 25, 30, 0},
 	}
-	n := 4
+	numCities := len(graph)
 	// bitmask for visited cities
-	visited := (1 << n) - 1
-	r, c := visited+1, n
+	visited := (1 << numCities) - 1
+	rows := visited + 1
 	// Setup dp table
-	dp := make([][]int, r)
+	dp := make([][]int, rows)
 	for i := range dp {
-		dp[i] = make([]int, c)
+		dp[i] = make([]int, numCities)
 		for j := range dp[i] {
 			dp[i][j] = -1
 		}
 	}
 
-	result := tsp(1, 0, graph, dp, n, visited)
+	result := tsp(1, 0, graph, dp, numCities, visited)
 	fmt.Println(result)
 }
 
 // mask is the bitmask for visited cities
-// pos is the current city
-// graph is the adjacency matrix
+// currentCity is the current city
+// graph is the distance matrix
 // dp is the memoization table
 // n is the number of cities
-// visited is the bitmask for visited cities
-func tsp(mask, pos int, graph [][]int, dp [][]int, n, visited int) int {
+// visited is the bitmask for end state when all cities are visited
+func tsp(mask, currentCity int, graph [][]int, dp [][]int, n, visited int) int {
 	if mask == visited {
-		return graph[pos][0]
+		return graph[currentCity][0]
 	}
 	// If the result is already computed, return it
-	if dp[mask][pos] != -1 {
-		return dp[mask][pos]
+	if dp[mask][currentCity] != -1 {
+		return dp[mask][currentCity]
 	}
 
-	ans := math.MaxInt32
+	minCost := math.MaxInt32
 	for city := 0; city < n; city++ {
 		// If the city is not visited, compute the cost of visiting it
 		if (mask & (1 << city)) == 0 {
 			// Compute the cost of visiting the city and the cost of returning to the starting city
-			new := graph[pos][city] + tsp(mask|(1<<city), city, graph, dp, n, visited)
-			ans = min(ans, new)
+			visitCost := graph[currentCity][city] + tsp(mask|(1<<city), city, graph, dp, n, visited)
+			minCost = min(minCost, visitCost)
 		}
 	}
 
 	// Store the result in the memoization table
-	dp[mask][pos] = ans
-	return dp[mask][pos]
+	dp[mask][currentCity] = minCost
+	return dp[mask][currentCity]
 }
