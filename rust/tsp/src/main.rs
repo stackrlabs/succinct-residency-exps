9f16f2c7a762522bf9e537d111be1d11ef4cd554
Aashutosh Rathi <aashutosh@stackrlabs.xyz>
2024-10-03 12:11:45 -0700

    refactor: tsp

--- rust/tsp/src/main.rs
+++ rust/tsp/src/main.rs
@@ -5,45 +5,46 @@ fn main() {
         vec![15, 35, 0, 30],
         vec![20, 25, 30, 0],
     ];
-    let n = 4;
+    let num_of_cities = graph.len();
     // bitmask for visited cities
-    let visited = (1 << n) - 1;
-    let r = visited + 1;
-    let c = n;
+    let visited = (1 << num_of_cities) - 1;
+    let rows: usize = visited + 1;
     // Setup dp table
-    let mut dp = vec![vec![-1; c]; r];
+    let mut dp = vec![vec![-1; num_of_cities]; rows];
 
-    let result = tsp(1, 0, &graph, &mut dp, n, visited);
+    let result = tsp(1, 0, &graph, &mut dp, num_of_cities, visited);
     println!("{}", result);
+    println!("{:?}", dp);
 }
 
 // mask is the bitmask for visited cities
 // pos is the current city
-// graph is the adjacency matrix
+// graph is the distance matrix
 // dp is the memoization table
 // n is the number of cities
 // visited is the bitmask for visited cities
-fn tsp(mask: usize, pos: usize, graph: &Vec<Vec<i32>>, dp: &mut Vec<Vec<i32>>, n: usize, visited: usize) -> i32 {
+fn tsp(mask: usize, current_city: usize, graph: &Vec<Vec<i32>>, dp: &mut Vec<Vec<i32>>, n: usize, visited: usize) -> i32 {
     if mask == visited {
-        return graph[pos][0];
+        return graph[current_city][0];
     }
     // If the result is already computed, return it
-    if dp[mask][pos] != -1 {
-        return dp[mask][pos];
+    if dp[mask][current_city] != -1 {
+        return dp[mask][current_city];
     }
 
-    let mut ans = i32::MAX;
+    let mut min_cost = i32::MAX;
     for city in 0..n {
         // If the city is not visited, compute the cost of visiting it
         if (mask & (1 << city)) == 0 {
             // Compute the cost of visiting the city and the cost of returning to the starting city
-            let new = graph[pos][city] + tsp(mask | (1 << city), city, graph, dp, n, visited);
-            ans = ans.min(new);
+            // Note: this always leaves the even rows unchanged in the `dp` table, as we have last bit set. This can be optimized but left for simplicity
+            let visit_cost = graph[current_city][city] + tsp(mask | (1 << city), city, graph, dp, n, visited);
+            min_cost = min_cost.min(visit_cost);
         }
     }
 
     // Store the result in the memoization table
-    dp[mask][pos] = ans;
-    dp[mask][pos]
+    dp[mask][current_city] = min_cost;
+    dp[mask][current_city]
 }
 
