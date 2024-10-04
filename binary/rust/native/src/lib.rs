fd7ab7ccc91deac30bec9595cc07bdb7edb7cac6
prampey <prudhvi.rampey@gmail.com>
2024-10-04 14:14:09 -0700

    add base for native prime proving

---
+++ binary/rust/native/src/lib.rs
@@ -0,0 +1,16 @@
+pub fn binary_search(list: Vec<i32>, target: i32) -> bool {
+    let mut left = 0;
+    let mut right = list.len() - 1;
+    while left <= right {
+        let mid = (left + right) / 2;
+        if list[mid] == target {
+            return true;
+        }
+        if list[mid] < target {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    false
+}
---
+++ binary/rust/native_sp1/program/src/main.rs
@@ -0,0 +1,17 @@
+//! A simple program to be proven inside the zkVM.
+
+#![no_main]
+sp1_zkvm::entrypoint!(main);
+
+use binary::binary_search;
+
+pub fn main() {
+    let search_list = sp1_zkvm::io::read::<Vec<i32>>();
+    let search_value = sp1_zkvm::io::read::<i32>();
+    println!("cycle-tracker-start: execution");
+    let res = binary_search(search_list, search_value);
+    println!("cycle-tracker-end: execution");
+    println!("binary_search: found number: {}", res);
+
+    sp1_zkvm::io::commit(&res);
+}
