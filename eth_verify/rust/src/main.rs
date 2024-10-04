d9076a174abff240212ca2411fd6b9097ba83c38
Aashutosh Rathi <aashutosh@stackrlabs.xyz>
2024-10-04 12:35:19 -0700

    wip: rust fixes

--- eth_verify/rust/src/main.rs
+++ eth_verify/rust/src/main.rs
@@ -1,22 +1,28 @@
-use alloy_consensus;
-use alloy_primitives::keccak256;
-use alloy_rpc_types::Block;
+use alloy_primitives::{keccak256, B256};
 use alloy_rlp;
 use serde_json;
+use alloy_rpc_types::Block;
+use serde::{Serialize, Deserialize};
+
+#[derive(Deserialize, Serialize, Clone)]
+struct InputBlock {
+    header: Block,
+    hash: B256,
+}
 
 fn main() {
-    let file_path = "block_data.json";
+    let file_path = "../../inputs/block_data.json";
     let file_content = std::fs::read_to_string(file_path)
         .expect("Failed to read the file");
     let s = file_content.as_str();
-    let block = serde_json::from_str::<Block>(s).unwrap();
-    let result = verify_block_hash(block);
+    let block = serde_json::from_str::<InputBlock>(s).unwrap();
+    let result = verify_block(block.header, block.hash);
     println!("Block hash verification result: {}", result);
 }
 
-fn verify_block_hash(block: Block) -> bool {
+fn verify_block(block: Block, hash: B256) -> bool {
     let header: alloy_consensus::Header = block.clone().header.try_into().unwrap();
     let recomputed_hash = keccak256(alloy_rlp::encode(&header));
-    assert_eq!(recomputed_hash, block.header.hash);
-    recomputed_hash == block.header.hash
+    assert_eq!(recomputed_hash, hash);
+    recomputed_hash == hash
 }
