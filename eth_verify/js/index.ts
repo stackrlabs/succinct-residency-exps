eec986a8ce13d37c2ec1237d3edf333367df104a
prampey <prudhvi.rampey@gmail.com>
2024-10-07 15:40:51 -0700

    add MPT trie gen for native js

--- eth_verify/js/index.ts
+++ eth_verify/js/index.ts
@@ -1,6 +1,9 @@
+import { Trie } from "@ethereumjs/trie";
+import dotenv from "dotenv";
 import { rlp } from "ethereumjs-util";
 import fs from "fs/promises";
 import { keccak256 } from "js-sha3";
+import Web3 from "web3";
 
 interface Header {
   parentHash: string;
@@ -30,12 +33,23 @@ interface HeaderFromJSON {
 }
 
 async function main() {
-  const filePath = "../../inputs/block_data.json";
+  dotenv.config();
+  const web3 = new Web3(process.env.RPC_URL);
+  const filePath = "../../inputs/block.json";
   const fileContent = await fs.readFile(filePath, "utf-8");
   const jsonString = fileContent.toString();
 
-  const headerFromJSON: HeaderFromJSON = JSON.parse(jsonString);
-  const { header, hash } = headerFromJSON;
+  const blockData: { number: string; hash: string } = JSON.parse(jsonString);
+  const block = await web3.eth.getBlock(blockData.number, true);
+
+  const mptRoot = await calculateMptRoot(block);
+  console.log("Calculated MPT Root: " + mptRoot);
+  console.log("Block's MPT Root (TxHash): " + block.transactionsRoot);
+  if (mptRoot !== block.transactionsRoot) {
+    throw new Error("MPT Root does not match");
+  }
+  console.log("Block's MPT Root (TxHash): " + block.transactionsRoot);
+
   const {
     parentHash,
     sha3Uncles,
@@ -52,8 +66,7 @@ async function main() {
     extraData,
     mixHash,
     nonce,
-  } = header;
-
+  } = block;
   const blockHeader = [
     parentHash,
     sha3Uncles,
@@ -71,7 +84,7 @@ async function main() {
     mixHash,
     nonce,
   ];
-  verifyBlock(blockHeader, hash);
+  verifyBlock(blockHeader, blockData.hash);
 }
 
 main().catch((error) => console.error("An error occurred:", error));
@@ -86,5 +99,38 @@ function verifyBlock(blockHeader: any[], hash: string) {
     );
   }
 
-  console.log(`✅ Block Verified\nRecomputed hash: ${recomputedHash}`);
+  console.log(`Recomputed hash: ${recomputedHash}`);
+}
+
+async function calculateMptRoot(block: any) {
+  // Create a new trie
+  const trie = new Trie();
+
+  // Iterate over the transactions in the block
+  for (let i = 0; i < block.transactions.length; i++) {
+    const tx = block.transactions[i];
+
+    // Use transaction index as the key (RLP encoded)
+    const key = rlp.encode(i);
+
+    // RLP encode the transaction itself
+    const txData = rlp.encode([
+      tx.nonce,
+      tx.gasPrice,
+      tx.gas,
+      tx.to,
+      tx.value,
+      tx.input,
+      tx.v,
+      tx.r,
+      tx.s,
+    ]);
+
+    // Insert the transaction into the trie
+    await trie.put(key, txData);
+  }
+
+  // Get the root hash of the trie
+  const mptRoot = trie.root();
+  return "0x" + Buffer.from(mptRoot).toString("hex");
 }
