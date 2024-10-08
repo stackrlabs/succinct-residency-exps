9349f16750a0ae87f7ad14915013f9f45bbbcb96
Aashutosh Rathi <aashutosh@stackrlabs.xyz>
2024-10-08 16:21:05 -0700

    nexus basic fibonacci

---
+++ nth_fibonacci/nexus_zkvm/src/guest/src/main.rs
@@ -0,0 +1,18 @@
+#![no_std]
+#![no_main]
+
+#[nexus_rt::profile]
+fn fib(n: u32) -> u32 {
+    match n {
+        0 => 0,
+        1 => 1,
+        _ => fib(n - 1) + fib(n - 2),
+    }
+}
+ 
+#[nexus_rt::main]
+fn main() {
+    let n = 20;
+    let result = fib(n);
+    core::hint::black_box(result);
+}
