641550208d4b81866695ccd2c4f0db0ddc43f31a
prampey <prudhvi.rampey@gmail.com>
2024-10-09 03:43:48 -0700

    fix go to use block.json

--- eth_verify/go/main.go
+++ eth_verify/go/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -10,10 +9,8 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/ethereum/go-ethereum/trie"
-	"github.com/joho/godotenv"
 	"golang.org/x/crypto/sha3"
 )
 
@@ -23,33 +20,28 @@ type BlockFromJSON struct {
 }
 
 func main() {
-	// Connect to a local Geth node or a remote Ethereum node
-	godotenv.Load()
+	blockJSON, err := os.ReadFile("../../inputs/block.json")
+	if err != nil {
+		log.Fatalf("Failed to read block JSON from file: %v", err)
+	}
+	var block struct {
+		Transactions types.Transactions `json:"transactions"`
+		Hash         common.Hash        `json:"hash"`
+	}
+	err = json.Unmarshal(blockJSON, &block)
+	if err != nil {
+		log.Fatalf("Failed to unmarshal block JSON: %v", err)
+	}
+	transactions := block.Transactions
 
-	nodeURL := os.Getenv("ETH_NODE_URL")
-	if nodeURL == "" {
-		log.Fatalf("ETH_NODE_URL not set")
-	}
-	client, err := ethclient.Dial(nodeURL)
+	var header types.Header
+	headerJSON, err := os.ReadFile("../../inputs/block.json")
 	if err != nil {
-		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+		log.Fatalf("Failed to read header JSON from file: %v", err)
 	}
-
-	var blockData BlockFromJSON
-	blockNumberJSON, err := os.ReadFile("../../inputs/block.json")
+	err = json.Unmarshal(headerJSON, &header)
 	if err != nil {
-		log.Fatalf("Failed to read block number JSON from file: %v", err)
-	}
-	err = json.Unmarshal(blockNumberJSON, &blockData)
-	if err != nil {
-		log.Fatalf("Failed to unmarshal block number JSON: %v", err)
-	}
-	blockNumber := big.NewInt(blockData.Number)
-
-	// Fetch the block by number
-	block, err := client.BlockByNumber(context.Background(), blockNumber)
-	if err != nil {
-		log.Fatalf("Failed to retrieve block: %v", err)
+		log.Fatalf("Failed to unmarshal header JSON: %v", err)
 	}
 
 	// Create a new trie
@@ -59,7 +51,7 @@ func main() {
 	}
 
 	// Iterate over the transactions in the block
-	for i, tx := range block.Transactions() {
+	for i, tx := range transactions {
 		// Get the transaction index as a key (RLP encoded)
 		key, err := rlp.EncodeToBytes(big.NewInt(int64(i)))
 		if err != nil {
@@ -86,14 +78,14 @@ func main() {
 	fmt.Printf("Calculated MPT Root: %s\n", mptRoot.Hex())
 
 	// Compare with the block's official TxHash root (should match)
-	fmt.Printf("Block's MPT Root (TxHash): %v\n", block.Header().TxHash)
+	fmt.Printf("Block's MPT Root (TxHash): %v\n", header.TxHash)
 
-	if mptRoot != block.Header().TxHash {
+	if mptRoot != header.TxHash {
 		log.Fatalf("MPT Root does not match block's TxHash")
 		return
 	}
 
-	result := verifyBlock(*block.Header(), blockData.Hash)
+	result := verifyBlock(header, block.Hash)
 	if !result {
 		log.Fatalf("Block failed to verify")
 		return
