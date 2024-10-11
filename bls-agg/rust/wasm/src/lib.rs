afb07bd58e3032866a8f85344bee61fdd7af5244
prampey <prudhvi.rampey@gmail.com>
2024-10-11 14:09:53 -0700

    rewrote bls-agg and proved on native sp1

--- bls-agg/rust/wasm/src/lib.rs
+++ bls-agg/rust/wasm/src/lib.rs
@@ -1,33 +1,121 @@
-use blst::min_pk::{AggregateSignature, SecretKey, Signature};
+use bls12_381::{
+    hash_to_curve::{ExpandMsgXmd, HashToCurve, HashToField},
+    G2Affine,G2Projective, Scalar,
+};
+use sha2::{digest::generic_array::typenum::U48, digest::generic_array::GenericArray, Sha256};
+use hkdf::Hkdf;
+use thiserror::Error;
 
 #[no_mangle]
 pub fn bls_aggregate(num_signers: u32) -> u32 {
-    let mut private_key_bytes = Vec::new();
-    for i in 0..num_signers {
-        let sk_bytes = [i as u8; 32];
-        private_key_bytes.push(sk_bytes);
+    let private_keys: Vec<_> = (0..num_signers)
+            .map(|i| PrivateKey::new(&[i as u8; 32]))
+            .collect();
+
+        // generate messages
+        let messages: Vec<Vec<u8>> = (0..num_signers)
+            .map(|_| "message".as_bytes().to_vec())
+            .collect();
+
+        // sign messages
+        let sigs = messages
+            .iter()
+            .zip(&private_keys)
+            .map(|(message, pk)| pk.sign(message))
+            .collect::<Vec<Signature>>();
+
+        let aggregated_signature = aggregate(&sigs).expect("failed to aggregate");
+        println!("aggregated_signature: {:?}", aggregated_signature);
+        1
+}
+
+#[derive(Debug)]
+pub struct Signature(G2Affine);
+
+impl From<G2Projective> for Signature {
+    fn from(val: G2Projective) -> Self {
+        Signature(val.into())
+    }
+}
+
+/// Aggregate signatures by multiplying them together.
+/// Calculated by `signature = \sum_{i = 0}^n signature_i`.
+pub fn aggregate(signatures: &[Signature]) -> Result<Signature, Error> {
+    if signatures.is_empty() {
+        return Err(Error::ZeroSizedInput);
     }
 
-    let mut signatures = Vec::new();
-    let message = b"42";
+    let res = signatures
+        .iter()
+        .fold(G2Projective::identity(), |acc, signature| acc + signature.0);
 
-    // Generate public keys and sign the message using hardcoded private keys
-    for sk_bytes in private_key_bytes.iter() {
-        let sk = SecretKey::key_gen(sk_bytes, &[]).unwrap();
+    Ok(Signature(res.into()))
+}
 
-        // Sign the message
-        let dst = b"BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_";
-        let sig = sk.sign(message, dst, &[]);
 
-        // Accumulate the signatures
-        signatures.push(sig);
+pub struct PrivateKey(pub(crate) Scalar);
+impl PrivateKey {
+    /// Generate a deterministic private key from the given bytes.
+    ///
+    /// They must be at least 32 bytes long to be secure, will panic otherwise.
+    pub fn new<T: AsRef<[u8]>>(msg: T) -> Self {
+        PrivateKey(key_gen(msg))
     }
 
-    // Aggregate the signatures
-    let aggregated_signature = AggregateSignature::aggregate(&signatures.iter().collect::<Vec<&Signature>>(), true)
-        .expect("Failed to aggregate signatures");
+    /// Sign the given message.
+    /// Calculated by `signature = hash_into_g2(message) * sk`
+    pub fn sign<T: AsRef<[u8]>>(&self, message: T) -> Signature {
+        let mut p = hash(message.as_ref());
+        p *= self.0;
 
-    println!("Aggregated signature: {:?}", aggregated_signature);
+        p.into()
+    }
+}
 
-    num_signers
+const CSUITE: &[u8] = b"BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_";
+
+pub fn hash(msg: &[u8]) -> G2Projective {
+    <G2Projective as HashToCurve<ExpandMsgXmd<sha2::Sha256>>>::hash_to_curve(msg, CSUITE)
+}
+
+fn key_gen<T: AsRef<[u8]>>(data: T) -> Scalar {
+    // "BLS-SIG-KEYGEN-SALT-"
+    const SALT: &[u8] = b"BLS-SIG-KEYGEN-SALT-";
+
+    let data = data.as_ref();
+    assert!(data.len() >= 32, "IKM must be at least 32 bytes");
+
+    // HKDF-Extract
+    let mut msg = data.as_ref().to_vec();
+    // append zero byte
+    msg.push(0);
+    let prk = Hkdf::<Sha256>::new(Some(SALT), &msg);
+
+    // HKDF-Expand
+    // `result` has enough length to hold the output from HKDF expansion
+    let mut result = GenericArray::<u8, U48>::default();
+    assert!(prk.expand(&[0, 48], &mut result).is_ok());
+
+    Scalar::from_okm(&result)
+}
+
+
+
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("Size mismatch")]
+    SizeMismatch,
+    #[error("Io error: {0}")]
+    Io(#[from] std::io::Error),
+    #[error("Group decode error")]
+    GroupDecode,
+    #[error("Curve decode error")]
+    CurveDecode,
+    #[error("Prime field decode error")]
+    FieldDecode,
+    #[error("Invalid Private Key")]
+    InvalidPrivateKey,
+    #[error("Zero sized input")]
+    ZeroSizedInput,
 }
