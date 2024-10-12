b25986997156af2c95c7f42df7e7a8ccd0d94658
prampey <prudhvi.rampey@gmail.com>
2024-10-11 17:23:31 -0700

    add delphinus

--- bls-agg/rust/delphinus/src/lib.rs
+++ bls-agg/rust/delphinus/src/lib.rs
@@ -3,11 +3,11 @@ use zkwasm_rust_sdk::{
     wasm_input,
     dbg,
 };
-use wasm::merkelize;
+use wasm::bls_aggregate;
 
 #[wasm_bindgen]
 pub fn zkmain() {
-    let num_leaves = unsafe { wasm_input(0) };
-    merkelize(num_leaves.try_into().unwrap());
+    let num_signatures = unsafe { wasm_input(0) };
+    bls_aggregate(num_signatures.try_into().unwrap());
     dbg!("done\n");
 }
