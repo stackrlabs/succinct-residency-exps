081d60940a788d3ad0f168fa641b939d17ec22c1
Aashutosh Rathi <aashutosh@stackrlabs.xyz>
2024-10-14 02:39:10 -0700

    add wasm_nexus with wasmi - erroneous

--- nth_prime/rust/wasm_nexus/src/guest/src/main.rs
+++ nth_prime/rust/wasm_nexus/src/guest/src/main.rs
@@ -1,32 +1,41 @@
 #![cfg_attr(target_arch = "riscv32", no_std, no_main)]
 extern crate alloc;
 use alloc::vec;
+use nexus_rt::{read_private_input};
+use wasmi::{Engine, Linker, Module, Store};
 
 #[nexus_rt::profile]
 fn nth_prime_exec(n: u32) -> u64 {
-    nth_prime(n)
+    let engine = Engine::default();
+    let module = Module::new(&engine, &mut &wasm[..]).expect("Failed to create module");
+
+    let linker = <Linker<u64>>::new(&engine);
+    let mut store = Store::new(&engine, input.clone());
+
+    let instance = linker
+        .instantiate(&mut store, &module)
+        .unwrap()
+        .start(&mut store)
+        .unwrap();
+
+    let nth_prime = instance
+        .get_typed_func::<u64, u64>(&mut store, "nth_prime")
+        .expect("Failed to get typed_func");
+    let res = nth_prime.call(&mut store, input).expect("Failed to call");
+    println!("The {}th prime is {}.", input, res);
+    res
 }
 
-// natively implemented so that can use alloc::vec
-// std::vec is not available in the scope
-fn nth_prime(n: u32) -> u64 {
-    if n == 0 {
-        return 0;
-    }
-    let mut primes = vec![2];
-    let mut candidate = 3;
-    while primes.len() < n as usize {
-        if primes.iter().all(|&p| candidate % p != 0) {
-            primes.push(candidate);
-        }
-        candidate += 2;
-    }
-    primes[n as usize - 1]
-}
  
 #[nexus_rt::main]
 fn main() {
-    let n = 1;
-    let result = nth_prime_exec(n);
-    assert_eq!(2, result);
+    let input = read_private_input::<(u32, &[u8])>().expect("failed to read input");
+ 
+    let mut res: u64 = 0;
+    if let Ok((n, wasm)) = input {
+        res = nth_prime_exec(n, wasm);
+        assert_eq!(541, res);
+    } else {
+        println!("No private input provided...");
+    }
 }
