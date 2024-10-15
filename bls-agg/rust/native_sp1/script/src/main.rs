692618a3dcf108d95d4eabd8329d7fa095b3027c
prampey <prudhvi.rampey@gmail.com>
2024-10-14 22:03:23 -0700

    add some cycles

--- bls-agg/rust/native_sp1/script/src/main.rs
+++ bls-agg/rust/native_sp1/script/src/main.rs
@@ -48,7 +48,7 @@ fn main() {
         // Generate the proof
         let proof = client
             .prove(&pk, stdin)
-            .compressed()
+            .groth16()
             .run()
             .expect("failed to generate proof");
 
