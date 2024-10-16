954ba5681b87c8919bc7c3c0f5f285a662ed5d9e
Aashutosh Rathi <aashutosh@stackrlabs.xyz>
2024-10-16 09:58:19 -0700

    bls delphinus base

--- bls-agg/rust/delphinus/test.sh
+++ bls-agg/rust/delphinus/test.sh
@@ -10,7 +10,7 @@ ZKWASM_DIR="../../../../zkWasm" # needs pre-installed
 ZKWASM_CLI=$ZKWASM_DIR/target/release/zkwasm-cli
 ZKWASM_PARAMS=$ZKWASM_DIR/params
 
-$ZKWASM_CLI --params $ZKWASM_PARAMS bls_aggregate setup --wasm ./pkg/output.wasm
-$ZKWASM_CLI --params $ZKWASM_PARAMS bls_aggregate dry-run --wasm ./pkg/output.wasm --output ./output --private 2:i64
-# time ($ZKWASM_CLI --params $ZKWASM_PARAMS merkle prove --wasm ./pkg/output.wasm --output ./output --private 5:i64)
-# $ZKWASM_CLI --params $ZKWASM_PARAMS merkle verify --wasm ./pkg/output.wasm --output ./output
+$ZKWASM_CLI --params $ZKWASM_PARAMS bls_aggregate
+$ZKWASM_CLI --params $ZKWASM_PARAMS bls_aggregate dry-run --wasm ./pkg/output.wasm --output ./output --private $1:i64
+time ($ZKWASM_CLI --params $ZKWASM_PARAMS bls_aggregate prove --wasm ./pkg/output.wasm --output ./output --private $1:i64)
+# $ZKWASM_CLI --params $ZKWASM_PARAMS bls_aggregate verify --wasm ./pkg/output.wasm --output ./output
--- bls-verify/rust/delphinus/src/lib.rs
+++ bls-verify/rust/delphinus/src/lib.rs
@@ -8,6 +8,7 @@ use wasm::bls_verify;
 #[wasm_bindgen]
 pub fn zkmain() {
     let num_signatures = unsafe { wasm_input(0) };
+    // TODO: make first argument a list of public keys
     bls_verify(num_signatures.try_into().unwrap());
     dbg!("done\n");
 }
