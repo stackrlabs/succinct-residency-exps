ed17e5d598ed66c675b43535b739c44c968b522b
Eshaan Bansal <eshaan7bansal@gmail.com>
2024-10-16 13:47:34 +0530

    use k256 directly in ecdsa_verify

--- ecdsa_verify/rust/wasm/src/lib.rs
+++ ecdsa_verify/rust/wasm/src/lib.rs
@@ -1,22 +1,26 @@
-#![no_std]
+use hex_literal::hex;
+use k256::ecdsa::{RecoveryId, Signature, VerifyingKey};
+use sha3::{Keccak256, Digest};
 
-use alloy_primitives::{address, Signature};
-use ark_std::str::FromStr;
-use ark_std::println;
-
-/// Source: https://github.com/succinctlabs/sp1/blob/6512b56296c2c5e53b10cce1a741173a3d2dde68/examples/patch-testing/program/src/main.rs#L126-L138
 #[no_mangle]
+/// Source: https://github.com/RustCrypto/elliptic-curves/blob/a4fadfd69be555110df1ca256dfe1023af84038a/k256/src/ecdsa.rs#L65-L92
 pub fn ecdsa_verify_n(n: u32) -> u32 {
-    let sig = Signature::from_str(
-        "b91467e570a6466aa9e9876cbcd013baba02900b8979d43fe208a4a4f339f5fd6007e74cd82e037b800186422fc2da167c747ef045e5d18a5f5d4300f8e1a0291c"
-    ).expect("could not parse signature");
-    let expected_address = address!("2c7536E3605D9C16a7a3D7b1898e529396a65c23");
+    let msg = b"example message";
+    let signature = Signature::try_from(hex!(
+        "46c05b6368a44b8810d79859441d819b8e7cdc8bfd371e35c53196f4bcacdb51
+         35c7facce2a97b95eacba8a586d87b7958aaf8368ab29cee481f76e871dbd9cb"
+    ).as_slice()).unwrap();
+    let recid = RecoveryId::try_from(1u8).unwrap();
+    let expected_key = VerifyingKey::from_sec1_bytes(
+        &hex!("0200866db99873b09fc2fb1e3ba549b156e96d1a567e3284f5f0e859a83320cb8b")
+    ).unwrap();
     for _ in 1..=n {
-        println!("cycle-tracker-start: k256 verify");
-        let recovered_address =
-            sig.recover_address_from_msg("Some data").expect("could not recover address");
-        println!("cycle-tracker-end: k256 verify");
-        assert_eq!(recovered_address, expected_address);
+        let recovered_key = VerifyingKey::recover_from_digest(
+            Keccak256::new_with_prefix(msg),
+            &signature,
+            recid
+        ).unwrap();
+        assert_eq!(recovered_key, expected_key);
     }
     1
 }
