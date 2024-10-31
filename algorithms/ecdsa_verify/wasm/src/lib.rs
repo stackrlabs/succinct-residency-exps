7541af7f1e1bd86d29dcd94dd8bf90a359cff774
prampey <prudhvi.rampey@gmail.com>
2024-10-31 14:18:25 -0700

    fix: ecdsa algo

--- algorithms/ecdsa_verify/wasm/src/lib.rs
+++ algorithms/ecdsa_verify/wasm/src/lib.rs
@@ -1,22 +1,24 @@
 #![no_std]
 
-use alloy_primitives::{address, Signature};
-use ark_std::str::FromStr;
-use ark_std::println;
+use hex;
+use k256::ecdsa::{signature::Verifier, Signature, VerifyingKey};
 
-/// Source: https://github.com/succinctlabs/sp1/blob/6512b56296c2c5e53b10cce1a741173a3d2dde68/examples/patch-testing/program/src/main.rs#L126-L138
+/// Source: https://github.com/risc0/risc0/blob/release-1.1/examples/ecdsa/methods/guest/src/bin/ecdsa_verify.rs
 #[no_mangle]
 pub fn ecdsa_verify_n(n: u32) -> u32 {
-    let sig = Signature::from_str(
-        "b91467e570a6466aa9e9876cbcd013baba02900b8979d43fe208a4a4f339f5fd6007e74cd82e037b800186422fc2da167c747ef045e5d18a5f5d4300f8e1a0291c"
-    ).expect("could not parse signature");
-    let expected_address = address!("2c7536E3605D9C16a7a3D7b1898e529396a65c23");
+    let sig_bytes: [u8; 64] = hex::decode(
+        "78f44a21d6711cc8771c73c5905b8661f0cda2370a04bc5c22c8884ab550d4c43fb3e92910e2ecdec67e486ab8444a51871925f88cca33d23470fb6eaa1d16dc"
+    ).expect("could not decode signature").try_into().expect("could not convert to array");
+    let sig = Signature::from_bytes(&sig_bytes.into()).expect("could not parse signature");
+    let verifying_key_bytes =
+        hex::decode("039b8327d929a0e45285c04d19c9fffbee065c266b701972922d807228120e43f3")
+            .expect("could not decode verifying key");
+    let verifying_key =
+        VerifyingKey::from_sec1_bytes(&verifying_key_bytes).expect("could not parse verifying key");
+    let msg = b"Hello, zkVM";
+
     for _ in 1..=n {
-        println!("cycle-tracker-start: k256 verify");
-        let recovered_address =
-            sig.recover_address_from_msg("Some data").expect("could not recover address");
-        println!("cycle-tracker-end: k256 verify");
-        assert_eq!(recovered_address, expected_address);
+        assert!(verifying_key.verify(msg, &sig).is_ok());
     }
     1
 }
