66dd2a6cce56439419bf7c0bf7aacd8159f80a78
prampey <prudhvi.rampey@gmail.com>
2024-11-01 15:17:21 -0700

    merkleize

---
+++ algorithms/merkle/native_powdr/src/main.rs
@@ -0,0 +1,34 @@
+use powdr::Session;
+use std::fs::File;
+use std::io::BufReader;
+use serde_json::Value;
+
+fn main() {
+    env_logger::init();
+
+    // Read the JSON file
+    let file = File::open("../../../inputs/merkle.json").expect("Failed to open input file");
+    let reader = BufReader::new(file);
+    let json: Value = serde_json::from_reader(reader).expect("Failed to parse JSON");
+
+    let input_value = json["numLeavesBase2"]
+        .as_i64()
+        .expect("Failed to parse value from JSON") as i32;
+    println!("Input value: {}", input_value);
+
+    // Create a new powdr session to make proofs for the `guest` crate.
+    // Store all temporary and final artifacts in `powdr-target`.
+    // Write `some_data` to channel 1 and the sum of `some_data` to channel 2.
+    // Any serde-serializable type can be written to a channel.
+    let mut session = Session::builder()
+        .guest_path("./guest")
+        .out_path("powdr-target")
+        .build()
+        .write(1, &input_value);
+
+    // Fast dry run to test execution.
+    session.run();
+
+    // Uncomment to compute the proof.
+    // session.prove();
+}
